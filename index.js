a5c98157eea7cb67124175f15e3252f672b3b2aa
brenocartaxo <155592397+brenocartaxo@users.noreply.github.com>
2024-05-29 22:59:33 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function scrolldivc() {
+    var elem = document.getElementById("contact");
+    elem.scrollIntoView();
+}
+function scrolldiva() {
+    var elem = document.getElementById("aboutus");
+    elem.scrollIntoView();
+}
+function scrolldivp() {
+    var elem = document.getElementById("pros");
+    elem.scrollIntoView();
+}
